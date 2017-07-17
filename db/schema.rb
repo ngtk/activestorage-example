b4ae11b9612cb080f79d018306ef718860bd19dc
Kent Nagata <ngtknt@me.com>
2017-07-17 19:47:45 +0900

    Exec `rails db:migrate`

---
+++ db/schema.rb
@@ -0,0 +1,40 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2017_07_17_104431) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name"
+    t.string "record_gid"
+    t.integer "blob_id"
+    t.datetime "created_at"
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_gid", "blob_id"], name: "index_active_storage_attachments_on_record_gid_and_blob_id", unique: true
+    t.index ["record_gid", "name"], name: "index_active_storage_attachments_on_record_gid_and_name"
+    t.index ["record_gid"], name: "index_active_storage_attachments_on_record_gid"
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key"
+    t.string "filename"
+    t.string "content_type"
+    t.text "metadata"
+    t.integer "byte_size"
+    t.string "checksum"
+    t.datetime "created_at"
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+end
